12c4c81dfb1f7b857703fac62dcfaea6ba4ce2d9
VanjaK89 <kavgic.vanja@gmail.com>
2022-03-29 15:01:54 +0200

    Initial commit

---
+++ src/main/java/at/campus02/iwi/pictures/Picture.java
@@ -0,0 +1,23 @@
+package at.campus02.iwi.pictures;
+
+
+ public class Picture {
+    public int width ;
+     public int length ;
+
+
+
+     public int totalPixels(){
+         return length * width;
+     }
+
+     public int totalSize(int bytePerPixel){
+         return bytePerPixel * totalPixels();
+     }
+     public void scale(double factor){
+         length = (int)(length * factor);
+         width *= factor;
+     }
+
+}
+
---
+++ src/main/java/at/campus02/iwi/pictures/PictureApp.java
@@ -0,0 +1,17 @@
+package at.campus02.iwi.pictures;
+
+public class PictureApp {
+    public static void main(String[] args) {
+        Picture p = new Picture();
+        p.width = 200;
+        p.length = 100;
+
+        System.out.println(p.totalPixels());
+
+        int totalSize = p.totalSize(2);
+        System.out.println(totalSize);
+        p.scale(0.5);
+        System.out.println(p.width + " " + p.length);
+
+    }
+}
